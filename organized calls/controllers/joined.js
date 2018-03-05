b2ce9e5b8c234c5597d6aa18eb9f706fd2835d15
Traskovs <agris.traskovs@accenture.com>
2018-03-05 10:18:35 +0200

    Example update how to use results from previous call to call another db query

--- organized calls/controllers/joined.js
+++ organized calls/controllers/joined.js
@@ -11,14 +11,15 @@ exports.findFirst = function(req, res){
         return res.send(combination);
       }else{
         //console.log(results);
+        //note - results may have more elements then the schema allows, but available will be only the schema elements
          combination.user = results;
-         Painter.find({style: {$in:['High Renaissance', 'Northen Renaissance']}}, function(err, results){
+         Painter.find({style: {$in:combination.user.artstyle}}, function(err, results2){
           if(err){
             console.log(err);
             return res.send(combination);
           }
           //console.log(results);
-          combination.Painters =  results;
+          combination.Painters =  results2;
           return res.send(combination);
         });
 
--- organized calls/models/users.js
+++ organized calls/models/users.js
@@ -6,6 +6,7 @@ var UserSchema = new Schema({
   name: String,
   password: String,
   profession: String,
+  artstyle: Array,
   id: Number
 });
 
